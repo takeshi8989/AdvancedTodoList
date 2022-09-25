1cfa9dc74647e2d8bb85bdf8bab1a1e09e855849
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-09-24 19:23:29 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
+      <Router basename="/AdvancedTodoList">
         <Navbar />
         <Switch>
           <Route exact path="/">
